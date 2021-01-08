2b04cffa3356dd893fd0df8b73861f59c45b67b0
lbavsc <471242574@qq.com>
2021-01-08 16:15:36 +0800

    Initial commit

---
+++ src/main/java/com/practice/ningbao/NingbaoApplication.java
@@ -0,0 +1,13 @@
+package com.practice.ningbao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NingbaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NingbaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/ningbao/NingbaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.ningbao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NingbaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
